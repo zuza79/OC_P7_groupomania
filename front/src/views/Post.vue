548e8348ef78bd82e18b76b60cdb4e65a0e38821
zuzana <zuzana.tahar@yahoo.fr>
2022-02-12 01:42:39 +0100

    vuejs

--- front/src/views/Post.vue
+++ front/src/views/Post.vue
@@ -9,33 +9,32 @@
                         <li>
                             <input type="text" v-model="titre" placeholder="Titre" size="50" required aria-label="Titre du post">
                         </li>
+                        
                         <li>
                             <textarea v-model="contenu" placeholder="Rédiger votre message..." rows="10" cols="60" required aria-label="Message du post"></textarea>
                         </li>
                         <li v-if="image">
-                            <img :src="image" alt="Image du post" class="file">
+                            <img :src="post.image" alt="Image du post" class="file">
                         </li>
                         <li>
-                            <label v-if="!image" for="file" class="label-file" aria-label="Choisir une photo pour ce post"></label>
-                            <button v-else @click="deletefile()" class="btnDelete label-file" aria-label="Supprimer cette photo du post"><i class="far fa-trash-alt"></i> Supprimer image</button>
-                            <input type="file" accept="image/jpeg, image/jpg, image/png, image/webp" v-on:change="uploadFile" id="file" class="input-file" aria-label="Image du post">
+                            <label v-if="!post.image" for="file" class="label-file" aria-label="Choisir une photo pour ce post"></label>
+                            <button v-else @click="deletefile()" class="label-file btnDelete" aria-label="Supprimer cette photo du post"><i class="far fa-trash-alt"></i> Supprimer image</button>
+                            <input type="file" accept=".jpeg, .jpg, .png, .webp, .gif" v-on:change="uploadFile" id="file" class="input-file" aria-label="Image du post">
                         </li>
-                        
+
                     </ul>
                 </form>
-                <button @click="createPost()" class="btnSave" aria-label="Envoyer le message">Envoyer</button>
+                <button @click="createPost()" class="btnSave" aria-label="Créer ce post">Valider</button>
                 <div>
-                <router-link to="/allposts" aria-label="Retour ver Le Flash Actu Groupomania"><i class="fas fa-home home"></i></router-link>
+                 <router-link to="/allposts" aria-label="Retour vers Le Flash Actu Groupomania"><i class="fas fa-home home"></i></router-link>
                 </div>
             </section>
-           
-            
         <Footer />
     </div>
 </template>
 
+
 <script>
-import axios from 'axios'
 import HeaderProfile from "../components/HeaderProfile";
 import Footer from "../components/Footer";
 
@@ -45,67 +44,87 @@ export default {
         HeaderProfile,
         Footer
     },
-    data () {
+    data() {
         return {
             titre: '',
             contenu: '',
-            image: null,
-            contentType: 'text',
+            image: '',
             preview: null
         }
     },
-    methods : {
-// Récupération du fichier image uploadé
-        onFileSelected(event) {
-            this.image = event.target.files[0]
+    methods: {
+        createPost() {
+            const Id = JSON.parse(localStorage.getItem("userId"))
+            const fileField = document.querySelector('input[type="file"]');
+            const token = JSON.parse(localStorage.getItem("token"))
+
+            if (this.titre === '')
+                alert("Veuillez remplir le titre")
+            if (this.contenu === '')
+                alert("Veuillez remplir le contenu du message")
+            if (this.image === '' && this.titre != '' && this.contenu != '') {
+                let data = new FormData()
+                data.append('title', this.titre)
+                data.append('content', this.contenu)
+                data.append('userId', Id)
+
+                axios.post("http://localhost:3000/api/posts", {
+                   
+                    headers: {
+                    'authorization': `Bearer ${token}`
+                    },
+                    body: data
+                })
+                .then((response) => {
+                    return response.json();
+                })
+                .then(() => {
+                    this.$router.push("/allposts");
+                })
+                .catch(alert)
+
+            } else if (this.titre != '' && this.contenu != '') {
+
+                var fileName = document.getElementById("file").value
+                var idxDot = fileName.lastIndexOf(".") + 1;
+                var extFile = fileName.substr(idxDot, fileName.length).toLowerCase();
+                
+                if (extFile === "jpg" || extFile === "jpeg" || extFile === "png" || extFile === "webp" ||extFile === "gif"){
+                    let data = new FormData()
+                    data.append('image', fileField.files[0])
+                    data.append('title', this.titre)
+                    data.append('content', this.contenu)
+                    data.append('userId', Id)
+
+                    axios.post("http://localhost:3000/api/posts", {
+                         headers: {
+                        'authorization': `Bearer ${token}`
+                        },
+                        body: data
+                    })
+                    .then((response) => response.json())
+                    .then(() => {
+                        this.$router.push("/allposts");
+                    })
+                    .catch(alert)
+                } else {
+                    alert("Uniquement les fichiers jpg, jpeg, png, webp et gif sont acceptés!");
+                }
+            }
         },
-
-        // Création d'un nouveau post
-        createPost() {    
-            const token = localStorage.getItem('token');
-            this.content = true;    
-            
-            // Aucun contenu ajouté
-            if(!this.content && !this.image){
-                this.content = false;
-
-            // Texte ajouté
-            } else if(this.contentType === "text"){
-                this.image = null
-                if(!this.content){
-                    this.content = false;
-                } else {
-                    const postText = { content: this.content }
-                    axios.post('http://localhost:3000/api/posts/', postText, {
-                        headers: {
-                            'authorization': `Bearer ${token}`,
-                        }
-                    })
-                        .then(res => console.log(res))
-                        .catch(() => console.log('Ceci est une erreur'))
+        uploadFile(e) {
+            if (e.target.files) {
+                let reader = new FileReader()
+                reader.onload = (event) => {
+                    this.preview = event.target.result
+                    this.image = event.target.result
                 }
-
-            // Image ajoutée
-            } else if(this.contentType === "img") {
-                this.content = "";
-                if (!this.image){
-                    this.content = false;
-                } else {
-                    let formData = new FormData();
-                    formData.append('image', this.image)
-                    axios.post('http://localhost:3000/api/posts/', formData, {
-                        headers: {
-                            'Content-Type': 'multipart/form-data',
-                            'authorization': `Bearer ${token}`,
-                        }
-                    })
-                        .then(() => {
-                            console.log('ok')
-                        })
-                        .catch((err) => console.log(err))
-                }
-            }            
-        }        
+                reader.readAsDataURL(e.target.files[0])
+            }
+        },
+        deletefile() {
+            this.image = '';
+        }
     }
 }
 </script>
